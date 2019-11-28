b31a8aecdcd005b571477b20bf530adab64524a1
Brayhan Criollo <brayhan.criollo@globant.com>
2019-11-28 17:26:14 -0500

    BaseProject

---
+++ src/main/java/academyapi/CoreAPI.java
@@ -0,0 +1,14 @@
+package academyapi;
+
+/**
+ * Base Project TestNG + Rest Assured
+ * Core API Methods
+ *
+ * @author Brayhan.Criollo
+ */
+public class CoreAPI {
+
+    public void apiExample() {
+        // Basic Test
+    }
+}
